0186e32165da58242004a471dc1eaf409a4fcce3
junsp <junsp@yahoo.com>
2024-07-30 00:00:53 -0700

    add tag to this commit..

--- goModulePractice.go
+++ goModulePractice.go
@@ -2,7 +2,7 @@ package goModulePractice
 
 import "fmt"
 
-// Hello returns a greeting for the named person.
+// Hello returns a greeting for the named person
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
